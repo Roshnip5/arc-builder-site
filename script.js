7f90097f40b0f59ee3068b5699af0c80ca85d5d8
Rosh_w3 <159819593+Roshnip5@users.noreply.github.com>
2026-06-20 23:28:48 +0530

    initial HTML structure for ARC Wallet Dashboard

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function playMusic(){
 document.getElementById("bgmusic").play();
 }
+function playMusic(){
+
+document.getElementById("music").play();
+
+}
+
+async function connectWallet(){
+
+if(window.ethereum){
+
+await window.ethereum.request({
+method:"eth_requestAccounts"
+});
+
+alert("Wallet Connected");
+
+}
+else{
+
+alert("Install MetaMask");
+
+}
+
+}
 
 async function connectWallet(){
 
