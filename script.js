eab5bfd65a9cc088862c2f9e128b9a8196f92ebb
Rosh_w3 <159819593+Roshnip5@users.noreply.github.com>
2026-06-20 20:14:57 +0530

    initial HTML structure for ARC Wallet Dashboard

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function checkWallet() {
-  const wallet = document.getElementById("wallet").value;
-
-  if (wallet === "") {
-    alert("Please enter a wallet address");
-  } else {
-    alert("Wallet Connected: " + wallet);
-  }
-}
+console.log("ARC Builder Loaded");
